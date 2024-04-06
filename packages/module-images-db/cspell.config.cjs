63429d91b900dcddbbff0a4659b62bf3d37fd548
akky-xxxx <114.s.aki@gmail.com>
2024-04-06 19:23:32 +0900

    chore(module-images-db): setup cspell

---
+++ packages/module-images-db/cspell.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  version: "0.2",
+  language: "en",
+  dictionaries: ["packages"],
+  dictionaryDefinitions: [
+    {
+      name: "packages",
+      path: "./config/cspell/packages.txt",
+    },
+  ],
+  ignorePaths: ["./packages/**"],
+}
