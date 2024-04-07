dfa023ec4cf11e0f77f339e990e44234dfe99a66
akky-xxxx <114.s.aki@gmail.com>
2024-04-07 13:26:53 +0900

    chore(service-main): setup cspell

---
+++ packages/service-main/cspell.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  version: "0.2",
+  language: "en",
+  dictionaries: ["colors", "packages"],
+  dictionaryDefinitions: [
+    {
+      name: "colors",
+      path: "./config/cspell/colors.txt",
+    },
+    {
+      name: "packages",
+      path: "./config/cspell/packages.txt",
+    },
+  ],
+}
