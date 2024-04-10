a86ce29d6ee8773f652c91fe68aea35c525fa703
akky-xxxx <114.s.aki@gmail.com>
2024-04-11 01:05:03 +0900

    feat(service-main-h): hands over the exteriorMode to _renderer

--- packages/service-main-h/app/routes/index.tsx
+++ packages/service-main-h/app/routes/index.tsx
@@ -1,7 +1,10 @@
+import { getCookie } from "hono/cookie"
 import { css } from "hono/css"
 import { createRoute } from "honox/factory"
 
 import Counter from "../islands/Counter"
+import { CookieKeys } from "../shared/const/CookieKeys"
+import { getExteriorMode } from "../shared/utils/getExteriorMode"
 
 const className = css`
   font-family: sans-serif;
@@ -9,11 +12,12 @@ const className = css`
 
 export default createRoute((c) => {
   const name = c.req.query("name") ?? "Hono"
+  const exteriorMode = getExteriorMode(getCookie(c, CookieKeys.ExteriorMode))
   return c.render(
     <div className={className}>
       <h1>Hello, {name}!</h1>
       <Counter />
     </div>,
-    { title: name },
+    { exteriorMode, title: name },
   )
 })
