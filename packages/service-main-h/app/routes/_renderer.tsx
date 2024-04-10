32f5a0d33b704cc0bef08685563f9e5285e07fd8
akky-xxxx <114.s.aki@gmail.com>
2024-04-10 22:21:59 +0900

    feat(service-main-h): specify reset css

--- packages/service-main-h/app/routes/_renderer.tsx
+++ packages/service-main-h/app/routes/_renderer.tsx
@@ -8,6 +8,10 @@ export default jsxRenderer(({ children, title }) => (
       <meta charset="utf-8" />
       <meta content="width=device-width, initial-scale=1.0" name="viewport" />
       <title>{title}</title>
+      <link
+        href="https://cdn.jsdelivr.net/npm/modern-css-reset/dist/reset.min.css"
+        rel="stylesheet"
+      />
       <Script async src="/app/client.ts" />
       <Style />
     </head>
