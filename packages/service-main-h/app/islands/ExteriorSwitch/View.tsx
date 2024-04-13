6caf8a2ff9d883ec2f29ddf433fb141e88ee1508
akky-xxxx <114.s.aki@gmail.com>
2024-04-14 00:10:39 +0900

    feat(service-main-h): use hooks at ExteriorSwitch

--- packages/service-main-h/app/islands/ExteriorSwitch/View.tsx
+++ packages/service-main-h/app/islands/ExteriorSwitch/View.tsx
@@ -7,26 +7,36 @@ import { inputStyle } from "./styles/inputStyle"
 import { labelStyle } from "./styles/labelStyle"
 import { switchStyle } from "./styles/switchStyle"
 
+import type { Props } from "./types/Props"
 import type { FC } from "hono/jsx"
 
 const IconSize = 20
 const { ICONS, ICON_DAY, ICON_NIGHT } = ClassNames
 
-export const View: FC = () => (
-  // TODO: jsx-a11y/label-has-associated-control の上書きがうまくいってない？
-  //  https://zenn.dev/krntmm/articles/756fdf2f8fcdff
-  // eslint-disable-next-line jsx-a11y/label-has-associated-control
-  <label className={labelStyle}>
-    <input className={inputStyle} name={ColorSchemaName} type="checkbox" />
-    <div className={switchStyle}>
-      <div className={ICONS}>
-        <span className={ICON_DAY}>
-          <SunIcon height={IconSize} width={IconSize} />
-        </span>
-        <span className={ICON_NIGHT}>
-          <MoonIcon height={IconSize} width={IconSize} />
-        </span>
+export const View: FC<Props> = (props) => {
+  const { isChecked, handleChange } = props
+  return (
+    // TODO: jsx-a11y/label-has-associated-control の上書きがうまくいってない？
+    //  https://zenn.dev/krntmm/articles/756fdf2f8fcdff
+    // eslint-disable-next-line jsx-a11y/label-has-associated-control
+    <label className={labelStyle}>
+      <input
+        checked={isChecked}
+        className={inputStyle}
+        name={ColorSchemaName}
+        type="checkbox"
+        onChange={handleChange}
+      />
+      <div className={switchStyle}>
+        <div className={ICONS}>
+          <span className={ICON_DAY}>
+            <SunIcon height={IconSize} width={IconSize} />
+          </span>
+          <span className={ICON_NIGHT}>
+            <MoonIcon height={IconSize} width={IconSize} />
+          </span>
+        </div>
       </div>
-    </div>
-  </label>
-)
+    </label>
+  )
+}
--- packages/service-main-h/app/islands/ExteriorSwitch/index.tsx
+++ packages/service-main-h/app/islands/ExteriorSwitch/index.tsx
@@ -1,9 +1,17 @@
+import { useExteriorSwitch } from "@islands/ExteriorSwitch/modules/useExteriorSwitch"
+
 import { View } from "./View"
 
+import type { ExteriorMode } from "@shared/types/ExteriorMode"
 import type { FC } from "hono/jsx"
 
-const ExteriorSwitch: FC = () => {
-  return <View />
+type Props = {
+  exteriorMode: ExteriorMode
+}
+
+const ExteriorSwitch: FC<Props> = (props) => {
+  const dependencies = useExteriorSwitch(props)
+  return <View {...dependencies} />
 }
 
 export default ExteriorSwitch
