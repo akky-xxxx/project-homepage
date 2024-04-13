ac7c59386cf8327afe54cd45aa036b0635562c4b
akky-xxxx <114.s.aki@gmail.com>
2024-04-14 01:46:33 +0900

    feat(service-main-h): use Images at /photo-gallery

--- packages/service-main-h/app/components/pages/PhotoGallery/index.tsx
+++ packages/service-main-h/app/components/pages/PhotoGallery/index.tsx
@@ -1,3 +1,12 @@
+import { ImagesDataBase } from "module-images-db/src"
+
+import { Images } from "@atoms/Images"
+
 import type { FC } from "hono/jsx"
 
-export const PhotoGallery: FC = () => <div>Photo Gallery</div>
+export const PhotoGallery: FC = () => (
+  <div>
+    <h2>Photo Gallery</h2>
+    <Images images={ImagesDataBase} />
+  </div>
+)
