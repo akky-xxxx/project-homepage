0cb646fc9d0922cac2b2a4d9dafae0bd6ca713f4
akky-xxxx <114.s.aki@gmail.com>
2024-04-13 22:19:46 +0900

    feat(module-images-db): define arrays by category

--- packages/module-images-db/src/index.ts
+++ packages/module-images-db/src/index.ts
@@ -1,4 +1,4 @@
-/* eslint-disable sonarjs/no-duplicate-string */
+/* eslint-disable sonarjs/no-duplicate-string, strict-check/forbidden-multiple-named-exports */
 import { sortImageDataBase } from "./modules/sortImageDataBase"
 import { sortTags } from "./modules/sortTags"
 
@@ -68,3 +68,9 @@ const ImagesDataBaseOrigin = [
 ] satisfies Readonly<ImagesDataBaseRecord[]>
 
 export const ImagesDataBase = [...ImagesDataBaseOrigin].sort(sortImageDataBase).map(sortTags)
+export const Locations = [...new Set(ImagesDataBase.map(({ area }) => area))]
+export const Tags = [...new Set(ImagesDataBase.flatMap(({ tags }) => tags))].sort()
+// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+export const Months = [...new Set(ImagesDataBase.map(({ date }) => date.slice(0, -3)))]
+  .sort()
+  .reverse()
