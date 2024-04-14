35b0010a652f37b47afc3dbcd14d374f4ff8fe8e
akky-xxxx <114.s.aki@gmail.com>
2024-04-14 23:57:07 +0900

    feat(service-main-h): adjust Heading2

--- packages/service-main-h/app/components/atoms/Heading2/index.tsx
+++ packages/service-main-h/app/components/atoms/Heading2/index.tsx
@@ -5,8 +5,8 @@ import { Spaces } from "@shared/styles/Spaces"
 
 import type { FcWithChildren } from "@shared/types/FcWithChildren"
 
-const { MEDIA_SP } = MediaQueries
-const { SPACE12 } = Spaces
+const { MEDIA_PC, MEDIA_SP } = MediaQueries
+const { SPACE04, SPACE08, SPACE12 } = Spaces
 
 export const Heading2: FcWithChildren = (props) => {
   const { children } = props
@@ -15,10 +15,14 @@ export const Heading2: FcWithChildren = (props) => {
 }
 
 const rootStyle = css`
-  padding-bottom: ${SPACE12}rem;
-  border-bottom: 1px solid var(--primary-color);
+  border-bottom: 0.2rem solid var(--primary-red);
+
+  ${MEDIA_PC} {
+    padding-bottom: ${SPACE12}rem;
+  }
 
   ${MEDIA_SP} {
-    padding-inline: ${SPACE12}rem;
+    padding-bottom: ${SPACE04}rem;
+    padding-inline: ${SPACE08}rem;
   }
 `
