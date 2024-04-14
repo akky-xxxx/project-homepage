cc629438bb6ba0bb19b7efd04332e368a38a6c21
akky-xxxx <114.s.aki@gmail.com>
2024-04-14 16:19:56 +0900

    refactor(service-main-h): remove unnecessary css property

--- packages/service-main-h/app/components/Layout/components/SideColumn/components/Menu/components/Items/styles/anchorStyle/index.ts
+++ packages/service-main-h/app/components/Layout/components/SideColumn/components/Menu/components/Items/styles/anchorStyle/index.ts
@@ -7,7 +7,6 @@ const { MEDIA_ONLY_HOVER } = MediaQueries
 export const anchorStyle = css`
   color: var(--primary-color);
   text-decoration: none;
-  transition: background-color 0.2s ease;
 
   ${MEDIA_ONLY_HOVER} {
     &:hover {
