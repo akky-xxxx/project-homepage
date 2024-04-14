52149626cb0d7e28e453ec1a0815ef0f1fd474c3
akky-xxxx <114.s.aki@gmail.com>
2024-04-14 16:11:59 +0900

    feat(service-main-h): implement atoms/Image

---
+++ packages/service-main-h/app/components/atoms/Image/index.tsx
@@ -0,0 +1,20 @@
+import { css } from "hono/css"
+
+import type { FC } from "hono/jsx"
+import type { ImagesDataBase } from "module-images-db/src"
+
+const { VITE_IMAGE_HOST: IMAGE_HOST } = import.meta.env
+
+type Props = Pick<(typeof ImagesDataBase)[number], "imageId">
+
+export const Image: FC<Props> = (props) => {
+  const { imageId } = props
+  const source = IMAGE_HOST ? `${IMAGE_HOST}/${imageId}.avif` : "https://placehold.jp/1980x1320.png"
+
+  return <img alt="" className={imageStyle} src={source} />
+}
+
+const imageStyle = css`
+  margin-inline: auto;
+  max-height: calc(100vh - 130px);
+`
