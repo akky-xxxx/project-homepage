567f460d5cb0494f84094860cd612188512d5277
akky-xxxx <114.s.aki@gmail.com>
2024-04-14 23:51:44 +0900

    refactor(service-main-h): replace blockStyle to Block

--- packages/service-main-h/app/components/pages/PhotoGallery/index.tsx
+++ packages/service-main-h/app/components/pages/PhotoGallery/index.tsx
@@ -1,13 +1,9 @@
-import { css } from "hono/css"
-
+import { Block } from "@atoms/Block"
 import { Heading2 } from "@atoms/Heading2"
 import { Images } from "@atoms/Images"
-import { Spaces } from "@shared/styles/Spaces"
 
 import type { FC } from "hono/jsx"
 import type { ImagesDataBaseRecord } from "module-images-db/src/types/ImagesDataBaseRecord"
-
-const { SPACE20 } = Spaces
 
 type Props = {
   images: ImagesDataBaseRecord[]
@@ -20,13 +16,9 @@ export const PhotoGallery: FC<Props> = (props) => {
     <div>
       <Heading2>Photo Gallery</Heading2>
 
-      <div className={blockStyle}>
+      <Block>
         <Images images={images} />
-      </div>
+      </Block>
     </div>
   )
 }
-
-const blockStyle = css`
-  margin-top: ${SPACE20}rem;
-`
