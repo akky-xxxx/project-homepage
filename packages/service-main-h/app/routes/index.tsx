dcbf325f4bd81aa8795e6ee1943dcdd616db80e2
akky-xxxx <114.s.aki@gmail.com>
2024-04-14 23:23:03 +0900

    refactor(service-main-h): remove unnecessary island component

--- packages/service-main-h/app/routes/index.tsx
+++ packages/service-main-h/app/routes/index.tsx
@@ -5,8 +5,6 @@ import { createRoute } from "honox/factory"
 import { CookieKeys } from "@shared/const/CookieKeys"
 import { getExteriorMode } from "@shared/utils/getExteriorMode"
 
-import Counter from "../islands/Counter"
-
 const className = css`
   font-family: sans-serif;
 `
@@ -17,7 +15,6 @@ export default createRoute((c) => {
   return c.render(
     <div className={className}>
       <h1>Hello, {name}!</h1>
-      <Counter />
     </div>,
     { exteriorMode, title: name },
   )
