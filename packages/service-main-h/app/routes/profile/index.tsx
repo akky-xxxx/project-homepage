61943d629cff0e7f52e42bbea2018dc6285f2bb7
akky-xxxx <114.s.aki@gmail.com>
2024-04-14 23:49:13 +0900

    refactor(service-main-h): adjust routes/profile

--- packages/service-main-h/app/routes/profile/index.tsx
+++ packages/service-main-h/app/routes/profile/index.tsx
@@ -5,12 +5,6 @@ import { CookieKeys } from "@shared/const/CookieKeys"
 import { getExteriorMode } from "@shared/utils/getExteriorMode"
 
 export default createRoute((c) => {
-  const name = c.req.query("name") ?? "Hono"
   const exteriorMode = getExteriorMode(getCookie(c, CookieKeys.ExteriorMode))
-  return c.render(
-    <div>
-      <h1>Hello, {name}!</h1>
-    </div>,
-    { exteriorMode, title: name },
-  )
+  return c.render(<h1>Hello!</h1>, { exteriorMode, title: "Profile" })
 })
