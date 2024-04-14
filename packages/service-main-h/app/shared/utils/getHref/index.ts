48adb7f013bb58f34d7d6e95be917a02fd4d7b97
akky-xxxx <114.s.aki@gmail.com>
2024-04-14 16:32:04 +0900

    feat(service-main-h): update getHref

--- packages/service-main-h/app/shared/utils/getHref/index.ts
+++ packages/service-main-h/app/shared/utils/getHref/index.ts
@@ -2,6 +2,7 @@ import { unreachable } from "@shared/utils/unreachable"
 
 import type { GetHref } from "@shared/utils/getHref/types/GetHref"
 
+// eslint-disable-next-line max-statements, complexity
 export const getHref: GetHref = (props) => {
   const { id } = props
 
@@ -9,6 +10,18 @@ export const getHref: GetHref = (props) => {
     case "PhotoDetail": {
       const { imageId } = props
       return `/photo/${imageId}`
+    }
+    case "PhotoLocationDetail": {
+      const { location } = props
+      return `/photo-gallery/location/${location}`
+    }
+    case "PhotoDateDetail": {
+      const { date } = props
+      return `/photo-gallery/date/${date}`
+    }
+    case "PhotoTagDetail": {
+      const { tag } = props
+      return `/photo-gallery/tag/${tag}`
     }
     case "PhotoGallery": {
       return "/photo-gallery"
--- packages/service-main-h/app/shared/utils/getHref/types/GetHref/index.ts
+++ packages/service-main-h/app/shared/utils/getHref/types/GetHref/index.ts
@@ -1,10 +1,22 @@
 type Props =
   | {
+      id: "PhotoDateDetail"
+      date: string
+    }
+  | {
       id: "PhotoDetail"
       imageId: string
     }
   | {
       id: "PhotoGallery"
+    }
+  | {
+      id: "PhotoLocationDetail"
+      location: string
+    }
+  | {
+      id: "PhotoTagDetail"
+      tag: string
     }
 
 export type GetHref = (props: Props) => string
