245b9121768d6102ef83c1cb4e0d70db7894857e
akky-xxxx <114.s.aki@gmail.com>
2024-04-14 21:25:09 +0900

    refactor(service-main-h): adjust getHref

--- packages/service-main-h/app/shared/utils/getHref/types/GetHref/index.ts
+++ packages/service-main-h/app/shared/utils/getHref/types/GetHref/index.ts
@@ -1,17 +1,21 @@
 import type { PhotoGalleryFilterKey } from "@shared/types/PhotoGalleryFilterKey"
 
 type Props =
-  {
+  | {
       id: "PhotoDetail"
       imageId: string
-    } | {
-      id: "PhotoGallery"
-    } | Record<Extract<PhotoGalleryFilterKey, "date">, string> & {
-      id: "PhotoDateDetail"
-    } | Record<Extract<PhotoGalleryFilterKey, "location">, string> & {
-      id: "PhotoLocationDetail"
-    } | Record<Extract<PhotoGalleryFilterKey, "tag">, string> & {
-      id: "PhotoTagDetail"
     }
+  | {
+      id: "PhotoGallery"
+    }
+  | (Record<Extract<PhotoGalleryFilterKey, "date">, string> & {
+      id: "PhotoDateDetail"
+    })
+  | (Record<Extract<PhotoGalleryFilterKey, "location">, string> & {
+      id: "PhotoLocationDetail"
+    })
+  | (Record<Extract<PhotoGalleryFilterKey, "tag">, string> & {
+      id: "PhotoTagDetail"
+    })
 
 export type GetHref = (props: Props) => string
