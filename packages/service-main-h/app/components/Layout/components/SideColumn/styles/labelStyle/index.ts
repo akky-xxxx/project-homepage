efab60a72a60814c76e8d24f0919204fa4bdb922
akky-xxxx <114.s.aki@gmail.com>
2024-04-16 00:30:36 +0900

    feat(service-main-h): define labelStyle

---
+++ packages/service-main-h/app/components/Layout/components/SideColumn/styles/labelStyle/index.ts
@@ -0,0 +1,33 @@
+import { css } from "hono/css"
+
+import { MediaQueries } from "@shared/styles/MediaQueries"
+import { Spaces } from "@shared/styles/Spaces"
+
+import { MenuIconSize } from "../../const/MenuIconSize"
+import { ZIndexes } from "../../const/ZIndexes"
+
+const { MEDIA_PC, MEDIA_SP } = MediaQueries
+const { SPACE12 } = Spaces
+
+export const labelStyle = css`
+  ${MEDIA_PC} {
+    display: none;
+  }
+
+  ${MEDIA_SP} {
+    background-color: pink; /* TODO: --secondary-background を定義して置き換える */
+    border-radius: 4px;
+    bottom: ${SPACE12}rem;
+    display: grid;
+    height: ${MenuIconSize}px;
+    place-content: center;
+    position: fixed;
+    right: ${SPACE12}rem;
+    width: ${MenuIconSize}px;
+    z-index: ${ZIndexes.LABEL};
+
+    & > input {
+      display: none;
+    }
+  }
+`
