32356f4b5e378b93bf810f0b9a4484e91a969746
akky-xxxx <114.s.aki@gmail.com>
2024-04-16 00:31:03 +0900

    feat(service-main-h): define navigationStyle

--- packages/service-main-h/app/components/Layout/components/SideColumn/styles/navigationStyle/index.ts
+++ packages/service-main-h/app/components/Layout/components/SideColumn/styles/navigationStyle/index.ts
@@ -1,9 +1,36 @@
 import { css } from "hono/css"
 
+import { MediaQueries } from "@shared/styles/MediaQueries"
 import { Spaces } from "@shared/styles/Spaces"
 
-const { SPACE20 } = Spaces
+import { MenuIconSize } from "../../const/MenuIconSize"
+import { ZIndexes } from "../../const/ZIndexes"
+
+const { SPACE12, SPACE20 } = Spaces
+const { MEDIA_PC, MEDIA_SP } = MediaQueries
 
 export const navigationStyle = css`
-  margin-top: ${SPACE20}rem;
+  ${MEDIA_PC} {
+    margin-top: ${SPACE20}rem;
+  }
+
+  ${MEDIA_SP} {
+    background-color: var(--primary-background);
+    bottom: calc(${SPACE12 * 2}rem + ${MenuIconSize}px);
+    box-shadow: 0 5px 5px rgba(0, 0, 0, 0.2); /* TODO: 定数定義して置き換える */
+    left: 0;
+    overflow-y: auto;
+    position: fixed;
+    right: 0;
+    top: 0;
+    z-index: ${ZIndexes.NAVIGATION};
+
+    label:has(:not(:checked)) + & {
+      display: none;
+    }
+
+    label:has(:checked) + & {
+      display: block;
+    }
+  }
 `
