e27563153d983596871b9a3cde13bb01228bc879
akky-xxxx <114.s.aki@gmail.com>
2024-04-18 01:29:57 +0900

    feat(service-main-h): use Conditions

--- packages/service-main-h/app/components/pages/PhotoGallery/index.tsx
+++ packages/service-main-h/app/components/pages/PhotoGallery/index.tsx
@@ -2,19 +2,25 @@ import { Block } from "@atoms/Block"
 import { Heading2 } from "@atoms/Heading2"
 import { Images } from "@atoms/Images"
 
+import { Conditions } from "./components/Conditions"
+
+import type { PhotoGalleryFilterKey } from "@shared/types/PhotoGalleryFilterKey"
 import type { FC } from "hono/jsx"
 import type { ImagesDataBaseRecord } from "module-images-db/src/types/ImagesDataBaseRecord"
 
 type Props = {
   images: ImagesDataBaseRecord[]
+  filterQueries: Partial<Record<PhotoGalleryFilterKey, string>>
 }
 
 export const PhotoGallery: FC<Props> = (props) => {
-  const { images } = props
+  const { images, filterQueries } = props
 
   return (
     <div>
       <Heading2>Photo Gallery</Heading2>
+
+      <Conditions filterQueries={filterQueries} />
 
       <Block>
         <Images images={images} />
