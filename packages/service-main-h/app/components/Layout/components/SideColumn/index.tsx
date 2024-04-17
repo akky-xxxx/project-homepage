9d6d7046324a9abb0d1451d9ae7ffdfa05c91b38
akky-xxxx <114.s.aki@gmail.com>
2024-04-17 23:37:15 +0900

    style(service-main-h): apply fix task

--- packages/service-main-h/app/components/Layout/components/SideColumn/index.tsx
+++ packages/service-main-h/app/components/Layout/components/SideColumn/index.tsx
@@ -9,21 +9,19 @@ import { rootStyle } from "./styles/rootStyle"
 
 import type { FC } from "hono/jsx"
 
-export const SideColumn: FC = () => {
-  return (
-    <div className={rootStyle}>
-      <header className={headerStyle}>
-        <h1 className={heading1Style}>akky-xxxx</h1>
-      </header>
-      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-      <label className={labelStyle}>
-        <input type="checkbox" />
-        <Hamburger />
-      </label>
-      <nav className={navigationStyle}>
-        <Menu />
-      </nav>
-      <div className={backdropStyle} />
-    </div>
-  )
-}
+export const SideColumn: FC = () => (
+  <div className={rootStyle}>
+    <header className={headerStyle}>
+      <h1 className={heading1Style}>akky-xxxx</h1>
+    </header>
+    {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+    <label className={labelStyle}>
+      <input type="checkbox" />
+      <Hamburger />
+    </label>
+    <nav className={navigationStyle}>
+      <Menu />
+    </nav>
+    <div className={backdropStyle} />
+  </div>
+)
