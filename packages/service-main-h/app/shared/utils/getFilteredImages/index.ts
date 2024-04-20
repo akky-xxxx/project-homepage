e916a68e5268fb09d8ed1d4d3b6aa9c90d5639ab
akky-xxxx <114.s.aki@gmail.com>
2024-04-20 15:45:21 +0900

    refactor(service-main-h): replace to FilterQueries

--- packages/service-main-h/app/shared/utils/getFilteredImages/index.ts
+++ packages/service-main-h/app/shared/utils/getFilteredImages/index.ts
@@ -2,15 +2,17 @@ import { isIncludesTag } from "./modules/isIncludesTag"
 import { isSameLocation } from "./modules/isSameLocation"
 import { isStartsWithDate } from "./modules/isStartsWithDate"
 
-import type { PhotoGalleryFilterKey } from "@shared/types/PhotoGalleryFilterKey"
+import type { FilterQueries } from "@shared/types/FilterQueries"
 import type { ImagesDataBaseRecord } from "module-images-db/src/types/ImagesDataBaseRecord"
 
 type GetFilteredImages = (
-  filterQueries: Record<PhotoGalleryFilterKey, string>,
+  filterQueries: FilterQueries,
 ) => (images: ImagesDataBaseRecord[]) => ImagesDataBaseRecord[]
 
 export const getFilteredImages: GetFilteredImages = (filterQueries) => (images) => {
   const { date, location, tag } = filterQueries
+  if (!date || !location || !tag) return images
+
   const isSameLocationMain = isSameLocation(location)
   const isStartsWithDateMain = isStartsWithDate(date)
   const isIncludesTagMain = isIncludesTag(tag)
