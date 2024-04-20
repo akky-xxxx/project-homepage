eed6be6435ada3c7288e53b2a3977841dd355560
akky-xxxx <114.s.aki@gmail.com>
2024-04-20 16:25:01 +0900

    chore(service-main-h): apply fix task

--- packages/service-main-h/vite.config.ts
+++ packages/service-main-h/vite.config.ts
@@ -1,19 +1,18 @@
-import pages from '@hono/vite-cloudflare-pages'
-import honox from 'honox/vite'
-import client from 'honox/vite/client'
-import { defineConfig, type UserConfig } from 'vite'
+import pages from "@hono/vite-cloudflare-pages"
+import honox from "honox/vite"
+import client from "honox/vite/client"
+import { defineConfig, type UserConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 const userConfig: UserConfig = {
   optimizeDeps: {
-    entries: [
-      "./app/routes/index.tsx",
-    ],
+    entries: ["./app/routes/index.tsx"],
   },
 }
 
+// eslint-disable-next-line import/no-default-export
 export default defineConfig(({ mode }) => {
-  const pluginsBase = mode === 'client' ? [client()] : [honox(), pages()]
+  const pluginsBase = mode === "client" ? [client()] : [honox(), pages()]
   const plugins = [...pluginsBase, tsconfigPaths()]
 
   return {
