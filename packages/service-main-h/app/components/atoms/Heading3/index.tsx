6731930178cb053e6831a699e7941e1ff2d94dfe
akky-xxxx <114.s.aki@gmail.com>
2024-04-20 16:18:28 +0900

    feat(service-main-h): adjust Heading3 css

--- packages/service-main-h/app/components/atoms/Heading3/index.tsx
+++ packages/service-main-h/app/components/atoms/Heading3/index.tsx
@@ -1,12 +1,10 @@
 import { css } from "hono/css"
 
-import { MediaQueries } from "@shared/styles/MediaQueries"
 import { Spaces } from "@shared/styles/Spaces"
 
 import type { FcWithChildren } from "@shared/types/FcWithChildren"
 
-const { MEDIA_PC, MEDIA_SP } = MediaQueries
-const { SPACE04, SPACE08 } = Spaces
+const { SPACE08 } = Spaces
 
 export const Heading3: FcWithChildren = (props) => {
   const { children } = props
@@ -17,14 +15,6 @@ export const Heading3: FcWithChildren = (props) => {
 const rootStyle = css`
   border-left-style: solid;
   border-left-color: var(--primary-red);
-
-  ${MEDIA_PC} {
-    border-left-width: 1rem;
-    padding-left: ${SPACE08}rem;
-  }
-
-  ${MEDIA_SP} {
-    border-left-width: 0.5rem;
-    padding-left: ${SPACE04}rem;
-  }
+  border-left-width: 1rem;
+  padding-left: ${SPACE08}rem;
 `
