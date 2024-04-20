1918d8c8d40bf278b51be56cc046809edb92cbd8
akky-xxxx <114.s.aki@gmail.com>
2024-04-21 01:27:29 +0900

    refactor(service-main-h): replace to pandacss at Images

--- packages/service-main-h/app/components/atoms/Images/styles/ulStyle/index.ts
+++ packages/service-main-h/app/components/atoms/Images/styles/ulStyle/index.ts
@@ -1,13 +1,8 @@
-import { css } from "hono/css"
+import { css } from "@panda/css"
 
-import { Spaces } from "@shared/styles/Spaces"
-import { ThumbnailWidth } from "@shared/styles/ThumbnailWidth"
-
-const { SPACE20 } = Spaces
-
-export const ulStyle = css`
-  display: grid;
-  gap: ${SPACE20}rem;
-  grid-template-columns: repeat(auto-fit, ${ThumbnailWidth}rem);
-  place-content: center;
-`
+export const ulStyle = css({
+  display: "grid",
+  gap: "{spacing.s20}",
+  gridTemplateColumns: "repeat(auto-fit, 30rem)",
+  placeContent: "center",
+})
