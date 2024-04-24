d7d88ae9665567e55dcd173580216cddcbe48d13
akky-xxxx <114.s.aki@gmail.com>
2024-04-24 17:39:27 +0900

    feat(service-main-h): add alt, className props to Image

--- packages/service-main-h/app/components/atoms/Image/index.tsx
+++ packages/service-main-h/app/components/atoms/Image/index.tsx
@@ -1,6 +1,3 @@
-import { imageStyle } from "./styles/imageStyle"
-import { thumbnailStyle } from "./styles/thumbnailStyle"
-
 import type { FC } from "hono/jsx"
 import type { ImagesDataBaseRecord } from "module-images-db/src/types/ImagesDataBaseRecord"
 
@@ -9,14 +6,15 @@ const { VITE_IMAGE_HOST: IMAGE_HOST } = import.meta.env
 const Placeholder = "https://placehold.jp/1980x1320.png"
 
 type Props = Pick<ImagesDataBaseRecord, "imageId"> & {
+  alt?: string
+  className?: Promise<string> | string
   isThumbnail?: boolean
 }
 
 export const Image: FC<Props> = (props) => {
-  const { imageId, isThumbnail } = props
+  const { alt = "", className, imageId, isThumbnail } = props
   const extension = isThumbnail ? ".thumb.avif" : ".avif"
-  const style = isThumbnail ? thumbnailStyle : imageStyle
   const source = IMAGE_HOST ? `${IMAGE_HOST}/${imageId}${extension}` : Placeholder
 
-  return <img alt="" class={style} decoding="async" src={source} />
+  return <img alt={alt} class={className} decoding="async" src={source} />
 }
