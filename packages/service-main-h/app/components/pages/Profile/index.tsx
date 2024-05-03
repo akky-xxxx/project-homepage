1306121bab7c665cc07709b5209fff49c15fe45e
akky-xxxx <114.s.aki@gmail.com>
2024-05-03 21:52:01 +0900

    feat(service-main-h): add skills to Profile

--- packages/service-main-h/app/components/pages/Profile/index.tsx
+++ packages/service-main-h/app/components/pages/Profile/index.tsx
@@ -2,8 +2,13 @@ import { Block } from "@atoms/Block"
 import { ExternalLink } from "@atoms/ExternalLink"
 import { Heading2 } from "@atoms/Heading2"
 import { Heading3 } from "@atoms/Heading3"
+import { Heading4 } from "@atoms/Heading4"
 import { Section } from "@atoms/Section"
 
+import { SkillList } from "./components/SkillList"
+import { Frameworks } from "./const/Frameworks"
+import { Infrastructures } from "./const/Infrastructures"
+import { Languages } from "./const/Languages"
 import { ulStyle } from "./styles/ulStyle"
 
 import type { FC } from "hono/jsx"
@@ -34,6 +39,39 @@ export const Profile: FC = () => (
           </ExternalLink>
         </p>
       </Block>
+    </Section>
+
+    <Section>
+      <Block>
+        <Heading3>Skills</Heading3>
+      </Block>
+
+      <Section>
+        <Block>
+          <Heading4>Languages</Heading4>
+        </Block>
+        <Block>
+          <SkillList skills={Languages} />
+        </Block>
+      </Section>
+
+      <Section>
+        <Block>
+          <Heading4>Libraries / Frameworks</Heading4>
+        </Block>
+        <Block>
+          <SkillList skills={Frameworks} />
+        </Block>
+      </Section>
+
+      <Section>
+        <Block>
+          <Heading4>Infrastructures</Heading4>
+        </Block>
+        <Block>
+          <SkillList skills={Infrastructures} />
+        </Block>
+      </Section>
     </Section>
 
     <Section>
