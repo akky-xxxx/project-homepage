4daf391d82141484c1a23753f873436ba8770188
akky-xxxx <114.s.aki@gmail.com>
2024-05-18 13:28:45 +0900

    feat(service-main-h): hands over the pagination data to PhotoGallery

--- packages/service-main-h/app/routes/photo-gallery/index.tsx
+++ packages/service-main-h/app/routes/photo-gallery/index.tsx
@@ -2,7 +2,10 @@ import { createRoute } from "honox/factory"
 import { ImagesDataBase } from "module-images-db/src"
 import { pick } from "remeda"
 
+import { ImagesPerPage } from "@shared/const/ImagesPerPage"
+
 import { PhotoGallery } from "../../components/pages/PhotoGallery"
+import { getPageData } from "../../modules/photoGallery/getPageData"
 import { getFilteredImages } from "../../shared/utils/getFilteredImages"
 
 import type { PhotoGalleryFilterKey } from "@shared/types/PhotoGalleryFilterKey"
@@ -11,10 +14,26 @@ const filterPickKeys = ["date", "location", "tag"] satisfies PhotoGalleryFilterK
 
 export default createRoute((c) => {
   const filterQueries = pick(c.req.query(), filterPickKeys)
-  const images = getFilteredImages(filterQueries)(ImagesDataBase)
-  return c.render(<PhotoGallery filterQueries={filterQueries} images={images} />, {
-    description: "趣味で撮った写真一覧",
-    filterQueries,
-    title: "Photo Gallery",
-  })
+  const filteredImages = getFilteredImages(filterQueries)(ImagesDataBase)
+
+  const { page } = c.req.query()
+  const { currentPage, totalPages } = getPageData(page || "", filteredImages.length)
+
+  // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+  const sliceStart = (currentPage - 1) * ImagesPerPage
+  const images = filteredImages.slice(sliceStart, sliceStart + ImagesPerPage)
+
+  return c.render(
+    <PhotoGallery
+      currentPage={currentPage}
+      filterQueries={filterQueries}
+      images={images}
+      totalPages={totalPages}
+    />,
+    {
+      description: "趣味で撮った写真一覧",
+      filterQueries,
+      title: "Photo Gallery",
+    },
+  )
 })
