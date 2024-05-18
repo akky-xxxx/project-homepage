b181dfbba7a1bd391e4c0ab14b75b3aa2e1abddc
akky-xxxx <114.s.aki@gmail.com>
2024-05-18 13:28:10 +0900

    feat(service-main-h): use Pagination at PhotoGallery

--- packages/service-main-h/app/components/pages/PhotoGallery/index.tsx
+++ packages/service-main-h/app/components/pages/PhotoGallery/index.tsx
@@ -1,20 +1,27 @@
+import { css } from "hono/css"
+
 import { Block } from "@atoms/Block"
 import { Heading2 } from "@atoms/Heading2"
 import { Images } from "@atoms/Images"
 
 import { Conditions } from "./components/Conditions"
+import { Pagination } from "./components/Pagination"
 
 import type { FilterQueries } from "@shared/types/FilterQueries"
 import type { FC } from "hono/jsx"
 import type { ImagesDataBaseRecord } from "module-images-db/src/types/ImagesDataBaseRecord"
 
 type Props = {
+  currentPage: number
   images: ImagesDataBaseRecord[]
   filterQueries: FilterQueries
+  totalPages: number
 }
 
+const ONE_PAGE = 1
+
 export const PhotoGallery: FC<Props> = (props) => {
-  const { images, filterQueries } = props
+  const { currentPage, images, filterQueries, totalPages } = props
 
   return (
     <div>
@@ -23,8 +30,29 @@ export const PhotoGallery: FC<Props> = (props) => {
       <Conditions filterQueries={filterQueries} />
 
       <Block>
+        {currentPage} of {totalPages} pages
+      </Block>
+
+      <Block>
         <Images filterQueries={filterQueries} images={images} />
       </Block>
+
+      {totalPages > ONE_PAGE && (
+        <Block>
+          <div class={paginationWrapperStyle}>
+            <Pagination
+              currentPage={currentPage}
+              filterQueries={filterQueries}
+              totalPages={totalPages}
+            />
+          </div>
+        </Block>
+      )}
     </div>
   )
 }
+
+const paginationWrapperStyle = css`
+  display: flex;
+  place-content: center;
+`
