faa0c6e4da7826efa32cdbde424361a1060cec18
akky-xxxx <114.s.aki@gmail.com>
2024-05-22 13:18:41 +0900

    feat(service-main-h): implement Heading2Inner

---
+++ packages/service-main-h/app/components/pages/PhotoGallery/components/Heading2Inner/index.tsx
@@ -0,0 +1,25 @@
+import { css } from "hono/css"
+
+import { MediaQueries } from "@shared/styles/MediaQueries"
+import { Spaces } from "@shared/styles/Spaces"
+
+import type { FC } from "hono/jsx"
+
+const { MEDIA_SP } = MediaQueries
+const { SPACE08 } = Spaces
+
+export const Heading2Inner: FC = (props) => {
+  const { children } = props
+
+  return <div class={rootStyle}>{children}</div>
+}
+
+export const rootStyle = css`
+  align-items: center;
+  column-gap: ${SPACE08}rem;
+  display: flex;
+
+  ${MEDIA_SP} {
+    justify-content: space-between;
+  }
+`
