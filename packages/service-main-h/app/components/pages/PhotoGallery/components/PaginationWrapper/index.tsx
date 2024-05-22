2120a5f86083e243032ce328127107bc4bdb62ae
akky-xxxx <114.s.aki@gmail.com>
2024-05-22 13:22:08 +0900

    feat(service-main-h): implement PaginationWrapper

---
+++ packages/service-main-h/app/components/pages/PhotoGallery/components/PaginationWrapper/index.tsx
@@ -0,0 +1,14 @@
+import { css } from "hono/css"
+
+import type { FC } from "hono/jsx"
+
+export const PaginationWrapper: FC = (props) => {
+  const { children } = props
+
+  return <div class={rootStyle}>{children}</div>
+}
+
+export const rootStyle = css`
+  display: flex;
+  place-content: center;
+`
