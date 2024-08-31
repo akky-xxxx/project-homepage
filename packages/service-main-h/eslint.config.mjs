3c4e031fe36888d179f569d4a51abdd67c4460ed
akky-xxxx <114.s.aki@gmail.com>
2024-08-31 22:38:35 +0900

    chore(service-main-h): setup eslint config for flat config

---
+++ packages/service-main-h/eslint.config.mjs
@@ -0,0 +1,41 @@
+/* eslint-disable import/no-relative-packages */
+import eslintConfigSCAll from "eslint-config-sc-all"
+import eslintPluginJsdoc from "eslint-plugin-jsdoc"
+import eslintPluginSonarjs from "eslint-plugin-sonarjs"
+
+import { ESLINT } from "../../config/eslint/ESLINT/index.mjs"
+import { IGNORES } from "../../config/eslint/IGNORES/index.mjs"
+import { IMPORT } from "../../config/eslint/IMPORT/index.mjs"
+import { JSDOC } from "../../config/eslint/JSDOC/index.mjs"
+import { PLUGIN_SC_JS } from "../../config/eslint/PLUGIN_SC_JS/index.mjs"
+import { SONARJS } from "../../config/eslint/SONARJS/index.mjs"
+import { TYPESCRIPT_ESLINT } from "../../config/eslint/TYPESCRIPT_ESLINT/index.mjs"
+
+/** @type {import("typescript-eslint").Config} */
+const config = [
+  IGNORES,
+
+  eslintPluginJsdoc.configs["flat/recommended-typescript"],
+  eslintPluginSonarjs.configs.recommended,
+  eslintConfigSCAll.getConfigs("typescript", ["react", "jest"]),
+
+  ESLINT,
+  PLUGIN_SC_JS,
+  TYPESCRIPT_ESLINT,
+  IMPORT,
+  SONARJS,
+  JSDOC,
+
+  {
+    rules: {
+      "react/no-unknown-property": [
+        2,
+        {
+          ignore: ["class", "for"],
+        },
+      ],
+    },
+  },
+].flat()
+
+export default config
