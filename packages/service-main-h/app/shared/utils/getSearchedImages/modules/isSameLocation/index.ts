807b4638f259761a863f8d8a93e5d6e51b1e87c3
akky-xxxx <114.s.aki@gmail.com>
2024-08-31 22:57:07 +0900

    refactor(service-main-h): resolve max-len errors

--- packages/service-main-h/app/shared/utils/getSearchedImages/modules/isSameLocation/index.ts
+++ packages/service-main-h/app/shared/utils/getSearchedImages/modules/isSameLocation/index.ts
@@ -1,3 +1,5 @@
 import type { ImagesDataBaseRecord } from "module-images-db/src/types/ImagesDataBaseRecord"
 
-export const isSameLocation = (location: string) => (record: ImagesDataBaseRecord) => (location ? record.area === location : true)
+export const isSameLocation = (location: string) => (record: ImagesDataBaseRecord) => (
+  location ? record.area === location : true
+)
--- packages/service-main-h/app/shared/utils/getSearchedImages/modules/isStartsWithDate/index.ts
+++ packages/service-main-h/app/shared/utils/getSearchedImages/modules/isStartsWithDate/index.ts
@@ -1,3 +1,5 @@
 import type { ImagesDataBaseRecord } from "module-images-db/src/types/ImagesDataBaseRecord"
 
-export const isStartsWithDate = (date: string) => (record: ImagesDataBaseRecord) => (date ? record.date.startsWith(date) : true)
+export const isStartsWithDate = (date: string) => (record: ImagesDataBaseRecord) => (
+  date ? record.date.startsWith(date) : true
+)
