c5e2a068818f76f11256f0ffda6bfb2ca9bc2772
akky-xxxx <114.s.aki@gmail.com>
2024-09-01 00:12:54 +0900

    refactor(service-main-h): resolve @typescript-eslint/naming-convention errors

--- packages/service-main-h/app/shared/const/CookieKeys/index.ts
+++ packages/service-main-h/app/shared/const/CookieKeys/index.ts
@@ -1,3 +1,6 @@
 export const CookieKeys = {
+  // const は upper came のルールなため
+  /* eslint-disable @typescript-eslint/naming-convention */
   ExteriorMode: "exterior-mode",
+  /* eslint-enable @typescript-eslint/naming-convention */
 }
--- packages/service-main-h/app/shared/const/ExteriorModes/index.ts
+++ packages/service-main-h/app/shared/const/ExteriorModes/index.ts
@@ -1,6 +1,9 @@
 import type { ExteriorMode } from "../../types/ExteriorMode"
 
 export const ExteriorModes = {
+  // const は upper came のルールなため
+  /* eslint-disable @typescript-eslint/naming-convention */
   Dark: "dark",
   Light: "light",
+  /* eslint-enable @typescript-eslint/naming-convention */
 } as const satisfies Record<string, ExteriorMode>
