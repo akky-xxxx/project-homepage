2cbdb85014db9baa6e8f1909e96f35eab3b0bbf9
akky-xxxx <114.s.aki@gmail.com>
2024-09-01 20:21:44 +0900

    refactor(module-images-db): resolve sonarjs/no-alphabetical-sort error

--- packages/module-images-db/src/modules/sortTags/index.ts
+++ packages/module-images-db/src/modules/sortTags/index.ts
@@ -1,4 +1,6 @@
+const compareFunction = (a: string, b: string) => a.localeCompare(b)
+
 export const sortTags = <T extends { tags: string[] }>(record: T) => ({
   ...record,
-  tags: [...new Set(record.tags)].sort(),
+  tags: [...new Set(record.tags)].sort(compareFunction),
 })
