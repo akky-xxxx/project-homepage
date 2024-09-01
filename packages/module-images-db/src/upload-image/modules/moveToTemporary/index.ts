4d14306509a81cd1232781b65254eef7f034d88d
akky-xxxx <114.s.aki@gmail.com>
2024-09-01 20:14:47 +0900

    refactor(module-images-db): resolve max-len error

--- packages/module-images-db/src/upload-image/modules/moveToTemporary/index.ts
+++ packages/module-images-db/src/upload-image/modules/moveToTemporary/index.ts
@@ -2,7 +2,10 @@ import fs from "fs"
 
 import type { FilesRecord } from "../../../shared/types/FilesRecord"
 
-export const moveToTemporary = (imageDirectory: string, temporaryDirectory: string) => (record: FilesRecord) => {
+export const moveToTemporary = (
+  imageDirectory: string,
+  temporaryDirectory: string,
+) => (record: FilesRecord) => {
   const { extension, fileName, id } = record
   fs.renameSync(
     `${imageDirectory}/${fileName}.${extension}`,
